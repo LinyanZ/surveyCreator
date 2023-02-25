355df2d9deb07e7d04b98c3e3bf1f6016e731465
linyanz <linyan.z@outlook.com>
2023-02-25 22:41:42 +1100

    minor styles change

--- frontend/src/pages/surveySubmissions.jsx
+++ frontend/src/pages/surveySubmissions.jsx
@@ -47,17 +47,23 @@ const Submission = ({ record }) => {
   return (
     <div className="relative py-2 my-4">
       <h3 className="w-full text-xl font-bold sm:text-2xl">{record.title}</h3>
-      <button
-        className="absolute right-0 top-2 text-xl sm:text-2xl"
-        onClick={() =>
-          setChartType(chartType === "barChart" ? "radarChart" : "barChart")
-        }
-      >
-        {chartType === "barChart" ? "Show Radar Chart" : "Show Bar Chart"}
-      </button>
+      {record.type !== SurveyTypes.ShortAnswer && (
+        <button
+          className="absolute right-0 top-2 text-xl sm:text-2xl h-8 w-8"
+          onClick={() =>
+            setChartType(chartType === "barChart" ? "radarChart" : "barChart")
+          }
+        >
+          {chartType === "barChart" ? (
+            <img src="/icons/hexagon.svg" />
+          ) : (
+            <img src="/icons/bar-chart.svg" />
+          )}
+        </button>
+      )}
       <div className="w-full h-[500px] my-8">
         {record.type === SurveyTypes.ShortAnswer ? (
-          <ul className="h-full overflow-auto">
+          <ul className="h-full overflow-auto text-lg sm:text-xl">
             {record.responses.map((r, i) => (
               <li key={`${record.qid} ${i}`}>{r}</li>
             ))}
@@ -77,6 +83,7 @@ const Submission = ({ record }) => {
               <RadarChart data={formatData(record)}>
                 <PolarGrid />
                 <PolarAngleAxis dataKey="option" />
+                <Tooltip />
                 <Radar
                   dataKey="count"
                   stroke="#8884d8"
